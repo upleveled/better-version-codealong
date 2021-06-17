becc837e4ed9f6b6fa6671198cdea9da96c60b1c
Karl Horky <karl.horky@gmail.com>
2021-06-17 16:22:34 +0200

    Add first version of script

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-// A fourth comment
+import { compareAsc, format } from 'date-fns';
+import { createRequire } from 'module';
 
-// A fifth comment
+const require = createRequire(import.meta.url);
+
+format(new Date(2014, 1, 11), 'yyyy-MM-dd');
+//=> '2014-02-11'
+
+const dates = [
+  new Date(1995, 6, 2),
+  new Date(1987, 1, 11),
+  new Date(1989, 6, 10),
+];
+dates.sort(compareAsc);
+console.log('These are the dates:', dates);
+//=> [
+//   Wed Feb 11 1987 00:00:00,
+//   Mon Jul 10 1989 00:00:00,
+//   Sun Jul 02 1995 00:00:00
+// ]
